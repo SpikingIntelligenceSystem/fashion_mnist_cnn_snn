8abf0bed496fd38fefd9578395ddb50855403288
Jack S. <jlogan2210@gmail.com>
2025-12-03 16:03:45 -0600

    various corrections to model code

--- models/csnn_single_step.py
+++ models/csnn_single_step.py
@@ -33,6 +33,7 @@ class FashionCSNN_SingleStep(nn.Module):
         x = self.conv2(x)
         x = self.lif2(x)
         x = self.pool(x)
+        x = x.view(x.size(0), -1)  # Flatten
         x = self.fc1(x)
         x = self.lif_conn(x)
         x = self.fc2(x)
